83ef8f974d403abe613d9440684ca32be93c06eb
Joshua Spears <j.dspears@yahoo.com>
2018-02-26 22:10:23 -0800

    Backend files are complete.

---
+++ app_api/controllers/article.ctrl.js
@@ -0,0 +1,23 @@
+// REQUIRE THE DATABASE MODELS
+const db = require("../models");
+
+// EXPORT ALL THE CONTROLLERS
+module.exports = {
+    
+    createArticle: (req, res) => {
+
+    }, // END CREATE
+
+    findAllArticle: (req, res) => {
+
+    }, // END FIND ALL
+
+    findOneArticle: (req, res) => {
+
+    }, // END FIND ONE
+
+    deleteEmployee: (req, res) => {
+
+    } // END DELETE
+    
+}; // END EXPORT
---
+++ app_api/routes/article.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const ctrl = require("../controllers/article.ctrl");
+// GRAP AND USE THE EXPRESS ROUTER 
+const router = express.Router();
+
+
+router.post("/article", ctrl.createArticle);
+
+router.get("/article", ctrl.findAllArticle);
+
+router.get("/article/:id", ctrl.findOneArticle);
+
+router.delete("/article/:id", ctrl.deleteArticle);
+
+
+module.exports = router;
