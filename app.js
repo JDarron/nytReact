4d68fef952563951ee67e727e027ad1444d65c12
Joshua Spears <j.dspears@yahoo.com>
2018-02-26 21:55:04 -0800

    Buiding the folder structure still

---
+++ app.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+const express = require("express");
+const bodyParser = require("body-parser");
+// DB MODELS
+const db = require("./app_api/models");
+// PORT
+const PORT = process.env.PORT || 3001;
+const app = express();
+// ROUTES FILE CONTAINING OUR ROUTES
+const routes = require("./app_api/routes/api/nyTimes.api");
+
+// BODY PARSER CONFIG
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('app_client/build'));
+
+// API ROUTES
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+}); // END THEN
