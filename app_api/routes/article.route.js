3abcd2a2f196e12b32f2e586d8011a6f8fa0d337
Joshua Spears <j.dspears@yahoo.com>
2018-03-26 18:15:29 -0700

    Testing the routes to make sure they are dependable.

--- app_api/routes/article.route.js
+++ app_api/routes/article.route.js
@@ -4,14 +4,16 @@ const ctrl = require("../controllers/article.ctrl");
 // GRAB AND USE THE EXPRESS ROUTER 
 const router = express.Router();
 
-
+// CREATE
 router.post("/article", ctrl.createArticle);
-
+// READ
 router.get("/article", ctrl.findAllArticle);
-
+// READ
 router.get("/article/:id", ctrl.findOneArticle);
-
+// UPDATE
+router.put("/article/:id", ctrl.updateArticle);
+// DELTE
 router.delete("/article/:id", ctrl.deleteArticle);
-
+// END C.R.U.D.
 
 module.exports = router;
