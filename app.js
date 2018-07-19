e36439ba138a3b3f69981fbf3d15cc29304dd17d
Joshua Spears <j.dspears@yahoo.com>
2018-07-19 12:53:41 -0700

    serving the public folder insted of the build.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 
 // serve up 'public' folder
-app.use(express.static('./app_client/build'));
+app.use(express.static('./app_client/public'));
 
 // =====================================================================================
 // MONGOOSE CONFIG
