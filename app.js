1b6268dee8a29128912f0b3abb84c09e51575754
Joshua Spears <j.dspears@yahoo.com>
2018-07-20 12:25:31 -0700

    changing the code back.

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const express = require('express')
     , bodyParser = require('body-parser')
     , logger = require('morgan')
     , mongoose = require('mongoose')
-    , apiRoutes = require('./app_api/routes/article.route')
-    , isDev = process.env.NODE_ENV === 'development';
+    , apiRoutes = require('./app_api/routes/article.route');
 
 // set port
 const port = process.env.PORT || 3001;
@@ -19,27 +18,15 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/nyTimesReact
 // initialize express
 const app = express();
 
-// // configure body parser to parse requests as json
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// // configure morgan to log requests to console
-// app.use(logger('dev'));
-
-// // serve up 'public' folder
-// app.use(express.static('./app_client/public'));
-app.use(bodyParser.json());
+// configure body parser to parse requests as json
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(logger('dev'));
-app.use(apiRoutes);
-if (isDev) {
-    app.use(express.static('app_client/public'));
-} else {
-    app.use(express.static('app_client/build'));
-}
+app.use(bodyParser.json());
 
+// configure morgan to log requests to console
+app.use(logger('dev'));
+
+// serve up 'public' folder
+app.use(express.static('./app_client/public'));
 
 // =====================================================================================
 // MONGOOSE CONFIG
