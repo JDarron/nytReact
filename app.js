7dbeb177d10b90b3919ac0494e04b4992ffe87ac
Joshua Spears <j.dspears@yahoo.com>
2018-07-25 12:13:10 -0700

    re added build

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ app.use(logger('dev'));
 // serve up 'public' folder
 // Serve static assets
 
-app.use(express.static(path.join(__dirname, './app_client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // =====================================================================================
 // MONGOOSE CONFIG
